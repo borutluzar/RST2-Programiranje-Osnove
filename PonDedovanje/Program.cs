1f6e08ea40a8f9c6698cdbd210074b09613e472f
Borut Lužar <borut.luzar@gmail.com>
2020-12-01 15:09:46 +0100

    Add project files.

---
+++ PonDedovanje/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PonDedovanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Monitor samsung = new Monitor(33, 1200);
+            Monitor dell = new Monitor(24, 1200);
+            Console.WriteLine($"Monitor {nameof(samsung)} ima diagonalo {samsung.Price}");
+
+            Vehicle voz = new Vehicle(2, 20);
+            Digger bager = new Digger(4, 185, true);
+            Console.WriteLine(bager.ToString());
+            Console.WriteLine($"Vozilo {nameof(bager)} ima maksimalno hitrost {bager.MaxSpeed}");
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Vehicle
+    {
+        //public Vehicle() { }
+
+        public Vehicle(int wheels, int maxSpeed)
+        {
+            this.Wheels = wheels;
+            this.MaxSpeed = maxSpeed;
+        }
+
+        public int Wheels { get; set; }
+
+        public int MaxSpeed { get; }
+
+        public override string ToString()
+        {
+            return $"Vozilo: W={this.Wheels}, MS={this.MaxSpeed}";
+        }
+    }
+
+    public class Digger : Vehicle
+    {
+        public Digger(int w, int m, bool drill) : base(w, m)
+        {
+            this.Drill = drill;
+        }
+
+        public bool Drill { get; set; }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, D={(this.Drill ? "DA" : "NE")}";
+        }
+    }
+
+
+    public class Monitor
+    {
+        public Monitor(double diag, double pr)
+        {
+            this.Diagonal = diag;
+            this.Price = pr;
+        }
+
+        public double Diagonal { get; set; }
+
+        public double Price { get; set; }
+    }
+}
---
+++ Ponavljanje/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Ponavljanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personA = new Person();
+
+            Person personB = new Person();
+            personB.Age = 33;
+            personB.Height = 192;
+
+            Person personC = personB;
+
+            //personA. = 21;
+            personA.SetAge(31);
+            
+            Console.WriteLine($"Oseba je stara {personC.Age}");
+        }
+    }
+
+    public class Person
+    {
+        public Person() { }
+
+        public Person(int age)
+        {
+            this.age = age;
+        }
+
+        public Person(int age, double weigth)
+        {
+            this.age = age;
+            this.weigth = weigth;
+        }
+
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+
+        public bool NauciSeProgramirati()
+        {
+            return false;
+        } 
+
+        // Spremenljivka (polje), privzeta vrednost 88,
+        // lahko ji hkrati spreminjamo vrednost in dostopamo do nje.
+        // Ne moremo omogočiti samo enega ali drugega.
+        public double weigth = 88;
+
+        // Lastnost s privzeto vrednostjo 180.
+        // Auto-implemented, pri get in set lahko
+        // določimo možnosti dostopa za vsakega posebej.
+        // npr. set samo protected.
+        public double Height { get; set; } = 180;
+
+        // Polna implementacija lastnosti,
+        // če ni dodatnih preverjanj ali omejitev,
+        // je koda kot spodaj in to lahko nadomestimo
+        // kar z auto-implemented lastnostjo,
+        // kjer se spremljajoči private field (age)
+        // samodejno zgenerira v ozadju.
+        private int age = 40;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+    }
+}
